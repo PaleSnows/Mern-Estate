78fda3429bb6634954c10e2abe99315939d07797
Renzaki <renzakithakuri@gmail.com>
2024-03-26 19:19:36 +0545

    connecting to databse

--- api/index.js
+++ api/index.js
@@ -1,8 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+dotenv.config();
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
-app.listen(3000,(req,res)=>{
-    console.log('Server running');
-})
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB!!!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(3000, (req, res) => {
+  console.log("Server running");
+});
