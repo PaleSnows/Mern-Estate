e6425479d0681dabe07ade7f994d1601bbf41461
Renzaki <renzakithakuri@gmail.com>
2024-03-26 21:38:13 +0545

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
 
 const app = express();
@@ -17,6 +18,7 @@ mongoose
   });
 
 app.use("/api/user", userRouter);
+app.use('/api/auth',authRouter)
 
 app.listen(3000, (req, res) => {
   console.log("Server running");
