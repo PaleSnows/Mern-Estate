8815d0b855ed933276721eb2454ac7f94b549301
Renzaki <renzakithakuri@gmail.com>
2024-03-28 18:11:26 +0545

    create user update api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,36 @@
-export const test=(req, res) => {
-    res.json({ message: "hello" });
+import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
+import bcrypt from "bcryptjs";
+
+export const test = (req, res) => {
+  res.json({ message: "hello" });
+};
+
+export const updateUser = async (req, res,next) => {
+  //we obtain user from verifyUser 
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(401, "You can only update your own account!"));
+  try {
+    if (req.body.password) {
+      req.body.password = bcrypt.hashSync(req.body.password, 10);
+    }
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      //set, used for condition when user updates only some fields or maybe all
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          avatar: req.body.avatar,
+        },
+      },
+      //new causes user to obtain new updated params rather than old ones
+      { new: true }
+    );
+    const { password, ...rest } = updatedUser._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
   }
+};
