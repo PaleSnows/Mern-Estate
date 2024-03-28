eb61ffb34ee3707e3f4800988296fd669b04a47f
Renzaki <renzakithakuri@gmail.com>
2024-03-28 21:41:28 +0545

    create listing api route

--- api/index.js
+++ api/index.js
@@ -3,12 +3,13 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
 dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
 mongoose
   .connect(process.env.MONGO)
@@ -21,6 +22,7 @@ mongoose
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
 //middleware ; use to register middlware
 app.use((err, req, res, next) => {
