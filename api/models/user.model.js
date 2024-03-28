0e665f80b189fb208bdaf9f2b6b4b302ec8c32c5
Renzaki <renzakithakuri@gmail.com>
2024-03-28 09:12:46 +0545

    completing OAuth page functionality

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -5,16 +5,21 @@ const userSchema = new mongoose.Schema(
     username: {
       type: String,
       required: true,
-      unique:true
+      unique: true,
     },
     email: {
       type: String,
       required: true,
-      unique:true
+      unique: true,
     },
     password: {
       type: String,
       required: true,
+    },
+    avatar: {
+      type: String,
+      default:
+        "https://wallpapers.com/images/featured/cool-profile-picture-87h46gcobjl5e4xu.jpg",
     },
   },
   { timestamps: true }
