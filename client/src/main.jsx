3d62c9f8644a444c5578bf138b8aba7ad087d6d8
Renzaki <renzakithakuri@gmail.com>
2024-03-30 17:23:54 +0545

    fix

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { store } from "./redux/store.js";
-import { persistor } from "./redux/store.js";
+import { persistor } from "../src/redux/store.js";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 
