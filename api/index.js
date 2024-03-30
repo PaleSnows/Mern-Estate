408d452cfca76d1caad9c69bd4e2079488635c07
Renzaki <renzakithakuri@gmail.com>
2024-03-30 16:21:44 +0545

    add pre installation

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
 dotenv.config();
+import path from 'path'
 
 const app = express();
 app.use(express.json());
@@ -20,9 +21,17 @@ mongoose
     console.log(err);
   });
 
+  const __dirname =path.resolve()
+
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname,'/client/dist')))
+
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(__dirname,'client','dist','index.html'))
+})
 
 //middleware ; use to register middlware
 app.use((err, req, res, next) => {
