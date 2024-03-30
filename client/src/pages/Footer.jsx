94e3eb3eecc1c10546333aa6186aae846b2bcba0
Renzaki <renzakithakuri@gmail.com>
2024-03-30 19:35:36 +0545

    final css

--- client/src/pages/Footer.jsx
+++ client/src/pages/Footer.jsx
@@ -4,37 +4,38 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div className=" items-center text-center gap-15   pt-12 text-slate-100 bg-black  h-[760px] sm:h-[360px]">
+    <div className=" items-center text-center gap-15   pt-12 text-slate-100 bg-black  h-[400px] sm:h-[360px]">
+
       <div className="mx-[1px] sm:mx-[200px]  sm:gap-1 flex mb-[70px]  flex-row  justify-between ">
-        <div className="ml-[9px] sm:ml-[1px]">
-          <h1 className="text-lg font-semibold mb-1 text-left">Renzaki Estate</h1>
+        <div className="ml-[5px] sm:ml-[1px]">
+          <h1 className="text-base sm:text-lg font-semibold mb-1 text-left">Renzaki Estate</h1>
           <div className="flex text-left flex-col gap-3">
-            <Link to={"/"}>Home</Link>
-            <Link to={"/about"}>About</Link>
-            <Link to={"/profile"}>Profile</Link>
-            <Link to={"/search"}>Search</Link>
+            <Link to={"/"} className="text-sm sm:text-base">Home</Link>
+            <Link to={"/about"} className="text-sm sm:text-base">About</Link>
+            <Link to={"/profile"} className="text-sm sm:text-base">Profile</Link>
+            <Link to={"/search"} className="text-sm sm:text-base">Search</Link>
           </div>
         </div>
-        <div className="">
-          <h1 className="text-lg font-semibold text-left mb-1">Contact </h1>
+        <div className="ml-[7px] sm:ml-[1px]">
+          <h1 className="text-base sm:text-lg font-semibold text-left mb-1">Contact </h1>
           <ul className="text-left flex flex-col gap-1 ">
-            <li className="">9898989898</li>
-            <li>estateeg@gmail.com</li>
+            <li className="text-sm sm:text-base">9898989898</li>
+            <li className="text-sm sm:text-base">ren@gmail.com</li>
           </ul>
         </div>
         <div className="ml-[17px] sm:ml-1">
-          <h1 className="text-lg text-left font-semibold mb-1">Company</h1>
+          <h1 className="text-base sm:text-lg text-left font-semibold mb-1">Company</h1>
           <ul className="text-left flex flex-col gap-2">
-            <li>Terms & Conditons</li>
-            <li>Privacy Policy</li>
-            <li>Disclaimer</li>
+            <li className="text-sm sm:text-base">Terms & Conditons</li>
+            <li className="text-sm sm:text-base">Privacy Policy</li>
+            <li className="text-sm sm:text-base">Disclaimer</li>
           </ul>
         </div>
         <div className="flex flex-col gap-2 mr-[5px] sm:mr-[1px] ">
-          <h1 className="text-lg text-right font-semibold mb-1">
+          <h1 className="text-base sm:text-lg text-right font-semibold mb-1">
             Get In Touch
           </h1>
-          <div className="flex flex-col sm:flex-row gap-3 ml-[60px] sm:ml-[1px] sm:text-right cursor-pointer ">
+          <div className="flex flex-col sm:flex-row gap-3 ml-[53px] sm:ml-[1px] sm:text-right cursor-pointer ">
             <FaFacebook  />
             <FaInstagram />
             <FaLinkedin />
